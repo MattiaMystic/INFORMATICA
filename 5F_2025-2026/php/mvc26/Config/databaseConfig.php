1409e088a69f55fe5b7861b38af6d9f13ee2022f
MattiaMystic <150335971+MattiaMystic@users.noreply.github.com>
2026-05-09 11:23:05 +0200

    Add files via upload

---
+++ 5F_2025-2026/php/mvc26/Config/databaseConfig.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'dns'=>'mysql:host=192.168.60.144;dbname=mattia_pavarin_tablet_store',
+    'username' => 'mattia_pavarin',
+    'password' => 'eroiche.scremammo.',
+    'options' => [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+];
